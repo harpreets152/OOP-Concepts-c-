5b26673795f74784e0343f191d56950b3846af69
harpreets152 <39302139+harpreets152@users.noreply.github.com>
2018-06-05 16:41:46 +0530

    Create SwitchCase.cpp

---
+++ SwitchCase.cpp
@@ -0,0 +1,17 @@
+
+C++ switch
+The C++ switch statement executes one statement from multiple conditions. It is like if-else-if ladder statement in C++.
+
+switch(expression){      
+case value1:      
+ //code to be executed;      
+ break;    
+case value2:      
+ //code to be executed;      
+ break;    
+......      
+      
+default:       
+ //code to be executed if all cases are not matched;      
+ break;    
+}    
